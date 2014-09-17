4eab2e52dc5cb8abe27ac08eeea25d48e18a57f5
Eric J Gruber <eric@rumblestrut.com>
2014-09-16 23:27:50 -0500

    Hide monthly service charge div for now

--- js/chartsdata.js
+++ js/chartsdata.js
@@ -47,3 +47,53 @@ $(function () {
                 }]
             });
         });
+
+// Monthly water service charge data
+$(function () {
+            $('#water-monthly-service-charge').highcharts({
+                title: {
+                    text: 'Water Service Charge',
+                    x: -20 //center
+                },
+                subtitle: {
+                    text: 'Source: lawrenceks.org',
+                    x: -20
+                },
+                xAxis: {
+                    categories: ['2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014']
+
+                },
+                yAxis: {
+                    title: {
+                        text: 'Price per 1,000 gallons'
+                    },
+                    plotLines: [{
+                        value: 0,
+                        width: 1,
+                        color: '#808080'
+                    }]
+                },
+                tooltip: {
+                    valuePrefix: '$'
+                },
+                legend: {
+                    layout: 'vertical',
+                    align: 'right',
+                    verticalAlign: 'middle',
+                    borderWidth: 0
+                },
+                series: [{
+                    name: 'Residential',
+                    data: [2.67, 2.78, 2.89, 2.92, 3.18, 3.52, 3.52, 3.78, 4.18, 4.51]
+                }, {
+                    name: 'Multifamily',
+                    data: [2.31, 2.40, 2.49, 2.60, 2.80, 3.06, 3.06, 3.29, 3.26, 3.33]
+                }, {
+                    name: 'Commercial',
+                    data: [2.05, 2.13, 2.22, 2.29, 2.58, 2.79, 2.79, 2.96, 3.33, 3.58]
+                }, {
+                    name: 'Industrial',
+                    data: [1.88, 1.94, 2.03, 2.22, 2.44, 2.72, 2.72, 2.87, 2.84, 3.14]
+                }]
+            });
+        });
